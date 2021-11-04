3c999fe60ec1d343a77854c1dc0c73a1c6a00977
admin <konstantinGridasov@gmail.com>
2021-11-05 00:04:47 +0300

    [story] Add security group and fixed with credentional

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,11 +1,43 @@
 provider "aws" {
+  region                  = "eu-central-1"
+  shared_credentials_file = "~/.aws/credentional"
+}
 
-  region = "us-central-1"
-  shared_credentials_file = "~/credentional/credentional_aws"
-   
- } 
-  
 
-resource "aws_instanse" "my_webserver" {
-  
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-09042b2f6d07d164a"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "My WebServer Security Group"
+  description = "My First Security Group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  # ingress {
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  #   # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  # }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+
 }
