d666ecaa99d9a0c79686575ea17573f1c270ca43
admin <konstantinGridasov@gmail.com>
2021-11-15 23:59:15 +0300

    [story] update info about terraform

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -3,30 +3,32 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentional"
 }
 
-resource "aws_eip" "my_static_ip" {
-  instance = aws_instance.my_webserver.id
-}
 
-resource "aws_instance" "my_webserver" {
+resource "aws_instance" "my_server" {
   ami                    = "ami-09042b2f6d07d164a"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  user_data              = file("data.sh")
 
   lifecycle {
     create_before_destroy = true
   }
+
 }
 
-resource "aws_security_group" "my_webserver" {
-  name        = "My WebServer Security Group"
-  description = "My First Security Group"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+resource "aws_security_group" "security_group" {
+  name        = "My WebServer Security Group"
+  description = "My Security Group"
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "8000"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
@@ -34,6 +36,5 @@ resource "aws_security_group" "my_webserver" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
   }
 }
--- terraform/data.sh
+++ terraform/data.sh
@@ -1,2 +1,7 @@
 #!/bin/bash/
-apt-get update
+sudo apt-get -y update 
+sudo apt-get -y install python3-pip
+sudo git clone xxxx
+cd ~/Try_on/api
+sudo pip install -r requirements.txt 
+sudo python3 manage.py runserver 
